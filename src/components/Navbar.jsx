93f9a0541b39d52d063b5086f1711a9ffbb92e4b
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-12-06 06:46:27 +0000

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,8 @@ import { navLinks } from "../constants"
 
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt='futurebank' className="w-[124px] h-[32px]" />
@@ -22,6 +24,41 @@ const Navbar = () => {
         </li>
           ))}
       </ul>
+      
+{/* list for mobile devices */}
+      <div className="sm:hidden flex flex-1
+      justify-end items-center">
+        <img 
+        src={toggle ? close : menu}
+        alt="menu"
+        className="w-[28px] h-[28px]
+        object-contain"
+        // keeps the State up to date
+        onClick={() => setToggle((prev) => !prev)}
+        />
+{/* opens the mobile menu */}
+        <div
+        className={`${toggle ? "flex" : "hidden" } p-6
+        bg-black-gradient absolute top-20 right-0
+        mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+
+{/* new menu */}
+      <ul className="list-none flex flex-col justify-end items-center flex-1">
+        { navLinks.map((nav, index) => ( 
+        <li
+          key={nav.id}
+          className={`font-poppins font-normal cursor-pointer text-[16px]
+          ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
+        >
+          <a href={`#${nav.id}`}>
+            {nav.title}
+          </a>
+        </li>
+          ))}
+      </ul>
+        </div>
+      </div>
+ 
     </nav >
   )
 }
